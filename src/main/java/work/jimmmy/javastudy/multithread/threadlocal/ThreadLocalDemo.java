a2717285bf987f2004febdd2103af15c6b91cb69
shenzui5233 <zui.shen@outlook.com>
2023-01-30 18:36:34 +0800

    增加threadlocal场景2案例：线程内，多个方法共用threadlocal中的对象，避免多层次穿参

---
+++ src/main/java/work/jimmmy/javastudy/multithread/threadlocal/ThreadLocalDemo.java
@@ -0,0 +1,42 @@
+package work.jimmmy.javastudy.multithread.threadlocal;
+
+public class ThreadLocalDemo {
+    public static void main(String[] args) {
+        new Service1().process();
+    }
+}
+
+class Service1 {
+    public void process() {
+        User user = new User("牛哥");
+        UserContextHolder.holder.set(user);
+        new Service2().process();
+        UserContextHolder.holder.remove(); // 帮助gc，避免value内存泄漏
+    }
+}
+
+class Service2 {
+    public void process() {
+        System.out.println("Service2: " + UserContextHolder.holder.get().name);
+        // UserContextHolder.holder.remove();
+        new Service3().process();
+    }
+}
+
+class Service3 {
+    public void process() {
+        System.out.println("Service3: " + (UserContextHolder.holder.get() == null ? null : UserContextHolder.holder.get().name));
+    }
+}
+
+class UserContextHolder {
+    public static ThreadLocal<User> holder = new ThreadLocal<>();
+}
+
+class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
