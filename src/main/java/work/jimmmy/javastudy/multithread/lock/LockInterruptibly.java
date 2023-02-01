c6f1dc01a51e3ea6e32b9dd751f715bc349a99c0
shenzui5233 <zui.shen@outlook.com>
2023-02-02 04:22:54 +0800

    Loc.lockInterruptibly()方法示例

---
+++ src/main/java/work/jimmmy/javastudy/multithread/lock/LockInterruptibly.java
@@ -0,0 +1,43 @@
+package work.jimmmy.javastudy.multithread.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockInterruptibly implements Runnable {
+    private final Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        LockInterruptibly lockInterruptibly = new LockInterruptibly();
+        Thread t0 = new Thread(lockInterruptibly);
+        Thread t1 = new Thread(lockInterruptibly);
+        t0.start();
+        t1.start();
+
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // t0.interrupt(); // 睡眠期间被中断，并不固定
+        t1.interrupt(); // 获取锁期间被中断，并不固定
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "尝试获取锁");
+        try {
+            lock.lockInterruptibly();
+            try {
+                System.out.println(Thread.currentThread().getName() + "获取到了锁");
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread() + "睡眠期间被中断了");
+            } finally {
+                lock.unlock();
+                System.out.println(Thread.currentThread() + "释放了锁");
+            }
+        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName() + "获得锁期间被中断了");
+        }
+    }
+}
