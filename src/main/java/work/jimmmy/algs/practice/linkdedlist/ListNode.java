8b122faf7239369d498fca13825517a30fe95713
shenzui5233 <zui.shen@outlook.com>
2023-03-17 11:59:37 +0800

    增加链表节点定义

---
+++ src/main/java/work/jimmmy/algs/practice/linkdedlist/ListNode.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) shenzui5233 2023-2023. All right reserved.
+ */
+
+package work.jimmmy.algs.practice.linkdedlist;
+
+import java.util.List;
+
+/**
+ * 链表节点
+ *
+ * @author shenzui5233
+ * @since 2023-03-16
+ */
+public class ListNode {
+    int val;
+
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
